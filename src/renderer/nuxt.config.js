d49ff9d5bf75101f7cb0a1bd3c070bb4519837ed
HarukiKinoshita <harukikinoshita@outlook.com>
2021-09-27 21:59:54 +0900

    Initialized

---
+++ src/renderer/nuxt.config.js
@@ -0,0 +1,80 @@
+const isProduction = process.env.NODE_ENV === 'production'
+const isDev = process.env.NODE_ENV === 'development'
+
+export default {
+  dev: isDev,
+
+  router: {
+    mode: 'hash',
+    trailingSlash: true
+  },
+
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: true,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'music-lounge',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    extend(config) {
+      if (!isDev) {
+        // config.output.path = __dirname + '_nuxt',
+        config.output.publicPath = './static/'      }
+      config.node = {
+        __dirname: !isProduction,
+        __filename: !isProduction
+      }
+    }
+  },
+
+  generate: {
+    dir: '../../dist/nuxt-build'
+  },
+  telemetry: false
+}
---
+++ src/renderer/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="bg-gray-100 px-auto p-10">
+    <NuxtLogo class="mx-auto py-4" />
+    <h1 class="text-2xl text-center font-light">Virtual Waterloo</h1>
+    <div class="flex flex-col items-center my-4">
+      <a class="px-4 py-2 border rounded-full bg-white hover:bg-gray-50" href="https://nuxtjs.org/ja/">Nuxt.js</a>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import NuxtLogo from '../components/NuxtLogo.vue'
+
+export default Vue.extend({
+  components: {
+    NuxtLogo
+  }
+})
+</script>
